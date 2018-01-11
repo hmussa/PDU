bcd357935261de2ca3fbe72603668eb2c02a8ccc
hmussa <hmussa@github.com>
2018-01-11 15:25:04 -0800

    updating script

--- pdu_script.py
+++ pdu_script.py
@@ -1,15 +1,13 @@
 # fullrun.py
 
-top_of_rack_switch_name = 'r07r13-tors.sjc23.lint'
-top_of_rack_switch_interface = 'Gi1/0/35'
-
-
+top_of_rack_switch_name = 'r07r12-tors.sjc23.lint'
+top_of_rack_switch_interface = 'Gi1/0/36'
 
 
 
 #find_this_IP = '00c0.b7c8.8295'
-location_of_rack = 'R07R13'
-Which_PDU = 'PDU1'
+location_of_rack = 'R07R12'
+Which_PDU = 'PDU2'
 #IP_for_infoblox = '100.119.12.118'
 #MAC_for_infoblox = '00c0.b7c8.8258'
 
@@ -18,26 +16,26 @@ mac_address = cmdrunner.get_mac_addr_from_TORS(top_of_rack_switch_name, top_of_r
 print ("The MAC Address is " + mac_address)
 
 import Core_script
-# Making a temporary hack to deal with the stpuid core outage we have all the time. BOO CORE.
-IP_address = '100.119.15.171'
-#IP_address = Core_script.get_IP_from_MAC(mac_address)
+#Making a temporary hack to deal with the stpuid core outage we have all the time. BOO CORE.
+#Bad_IP_address = '100.119.15.170'
+Bad_IP_address = Core_script.get_IP_from_MAC(MAC_for_infoblox)
+print ("The IP for this MAC " + Bad_IP_address)
 
-print ("The IP for this MAC " + IP_address)
+import Check_apc_type
+Check_model = Check_apc_type.checking_pw_model(Bad_IP_address)
+#print ("This PDU is " + Check_model)
 
 import csv_file
-Intended_IP = csv_file.get_intended_IP_add(location_of_rack, Which_PDU)
-print ("The intended IP is " + Intended_IP)
+Good_IP_address = csv_file.get_intended_IP_add(location_of_rack, Which_PDU)
+print ("The intended IP is " + Good_IP_address)
 
 from apceditinfoblox import editHostIPv4AddrForDHCPFromIP
-status = editHostIPv4AddrForDHCPFromIP (‘infoblox.lint’, ‘username’, ‘password’, IP_address, mac_address)
+status = editHostIPv4AddrForDHCPFromIP('infoblox.lint', 'hmussa', 'Ty91g3!6' ,Good_IP_address, mac_address)
 if status:
-  print (“Success!”)
+  print ("Success!")
 else:
-  print (“Fail….”)   
-
-import check_apc_type
-Check_model = check_apc_type.checking_pw_model(IP_address)
-print ("This PDU is " + Check_model)
+  print ("Fail")   
 
 
-#try and except 
+
+#reset after 
